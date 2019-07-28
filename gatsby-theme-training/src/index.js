bc43067da2fe9edf6d22961dd296ebf47c592c85
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-07-28 23:38:03 +0800

    add rollup build

---
+++ gatsby-theme-training/src/index.js
@@ -0,0 +1,3 @@
+export { default as Appbar } from './components/training/appbar';
+export { default as Button } from './components/training/button';
+export { default as Seo } from './components/training/seo';
